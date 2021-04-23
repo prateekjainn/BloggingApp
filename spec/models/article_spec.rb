d06c27ba214d7e9bd080954c8761b0a5b018e564
prateek jain <jprateek07@gmail.com>
2021-04-23 11:43:23 +0530

    added assoications test cases for article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-context "associations" do
+  context "associations" do
+    it "belongs to user " do
+      t = Article.reflect_on_association(:user)
+      expect(t.macro).to eq(:belongs_to)
+    end
+    it "has many likes" do
+      t = Article.reflect_on_association(:likes)
+      expect(t.macro).to eq(:has_many)
+    end
+    it "has many comments" do
+      t = Article.reflect_on_association(:comments)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
 end

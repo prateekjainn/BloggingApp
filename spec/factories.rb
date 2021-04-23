ba31f56b76ab4d07a75677bddd308bb8aca6e430
prateek jain <jprateek07@gmail.com>
2021-04-23 12:11:47 +0530

    added validations test cases for article entity

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,10 @@ FactoryBot.define do
       email {Faker::Internet.email}
       password {Faker::Internet.password}
     end
+
+    factory :article do
+      title {Faker::String.random(length: 3..50) }
+      description {Faker::String.random(length: 1..300) }
+      user_id { '97' }
+    end
   end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe Article, type: :model do
       expect(t.macro).to eq(:has_many)
     end
   end
+  context "validations" do
+    before do
+      # @user = User.new(username: "Any", email: "Lorem@iss.psum", password: "test")
+      @article= create(:article)
+      p @article
+    end
+    it 'has a article id' do
+      expect(@article.id).to be_present
+    end
+    it 'has a title' do
+      expect(@article.title).to be_present
+    end
+    it 'has a description' do
+      expect(@article.description).to be_present
+    end
+    it 'has a user id' do
+      expect(@article.user_id).to be_present
+    end
+    it 'has a username atleast 3 character long and less than 25 characters' do
+      expect(@article.title).to have_attributes(size: (be > 2) & (be <= 50)) # passes
+    end
+    it 'has a username atleast 3 character long and less than 25 characters' do
+      expect(@article.description).to have_attributes(size: (be > 9) & (be <= 300)) # passes
+    end
+  end
 end

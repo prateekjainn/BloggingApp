c6af3a3c1f5b42e184fb62d7be827bcb6a8c2c35
prateek jain <jprateek07@gmail.com>
2021-04-29 11:29:06 +0530

    added test case for likes controller

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,19 +2,13 @@ require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
   before(:each) do
-    # @user = create(:user)
-    # p @user
-    # login_as(@user)
-    # @article = create(:article, user: @user)
-    # p @article
-    @article=Article.find(105)
+    @user = create(:user)
+    @article = create(:article, user: @user)
   end
+
   context "POST #create" do
-    it 'should add a like to article' do
-      # @article.likes.create(user_id: @article.user_id)
-      @likes=create(:like)
-      # @likes=create(:like, article: @article, user: @article.user_id)
-      p @response
+    it 'should add a like to an article' do
+      @likes = Like.create(article_id: @article.id, user_id: @article.user_id)
       expect(response).to be_successful
     end
   end
